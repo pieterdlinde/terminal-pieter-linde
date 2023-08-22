5a0d4d0ae2326dfbfecca675a12ebea0e2093b6b
Pieter Linde <Pieter.Linde@rr-wfm.com>
2023-08-23 00:36:25 +0200

    Push Pieter

--- src/app/helpers/public/commands.ts
+++ src/app/helpers/public/commands.ts
@@ -132,9 +132,20 @@ export const sudo = async (args?: string[]): Promise<string> => {
 
 export const banner = (args?: string[]): string => {
   return `
-... (Your banner ASCII art) ...
+
+  /$$$$$$$  /$$              /$$                        
+  | $$__  $$|__/            | $$                        
+  | $$  \ $$ /$$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$ 
+  | $$$$$$$/| $$ /$$__  $$|_  $$_/   /$$__  $$ /$$__  $$
+  | $$____/ | $$| $$$$$$$$  | $$    | $$$$$$$$| $$  \__/
+  | $$      | $$| $$_____/  | $$ /$$| $$_____/| $$      
+  | $$      | $$|  $$$$$$$  |  $$$$/|  $$$$$$$| $$      
+  |__/      |__/ \_______/   \___/   \_______/|__/      
+                                                        
+
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the GitHub repository.
 `;
 };
+ 
