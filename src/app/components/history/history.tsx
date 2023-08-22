4f7eed88fc0c293aea1e9b6d3a2aae34583e99d2
Pieter Linde <pieterdlinde@gmail.com>
2023-08-22 07:23:10 +0200

    Push Test Changes

---
+++ src/app/components/history/history.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function History() {
+  return (
+    <div>This is the History</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import Image from 'next/image'
+"use client"
+import React from "react";
+import Input from "./components/input/input";
+import History from "./components/history/history";
 
 export default function Home() {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onClickAnywhere = () => {
+    inputRef?.current?.focus();
+  };
+
+
   return (
     <main>
-      <div>
-        Ready to rock
+      <div
+        className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base"
+        onClick={onClickAnywhere}
+      >
+        <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
+          <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
+            <div className="overflow-y-auto h-full">
+              <History></History>
+              <Input></Input>
+            </div>
+          </div>
+        </main>
       </div>
     </main>
   )
