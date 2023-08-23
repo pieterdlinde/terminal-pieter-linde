c4b15064ff27d8a611ee927910f22488f2fccc15
Pieter Linde <pieterdlinde@gmail.com>
2023-08-23 20:46:55 +0200

    Removed services

--- src/app/data/commands/quote.ts
+++ src/app/data/commands/quote.ts
@@ -1,4 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+
 export const quote = async (args: string[]): Promise<string> => {
-    const data = await getQuote();
-    return data.quote;
+  const data = await getQuote();
+  return data.quote;
+};
+
+interface QuoteData {
+  content: string;
+  author: string;
+  // Add other properties as needed
+}
+
+export const getQuote = async (): Promise<{ quote: string }> => {
+  const response: AxiosResponse<QuoteData> = await axios.get(
+    'https://api.quotable.io/random'
+  );
+  return {
+    quote: `“${response.data.content}” — ${response.data.author}`,
   };
+};
--- src/app/data/commands/weather.ts
+++ src/app/data/commands/weather.ts
@@ -1,8 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+
 export const weather = async (args: string[]): Promise<string> => {
-    const city = args.join('+');
-    if (!city) {
-      return 'Usage: weather [city]. Example: weather casablanca';
-    }
-    const weather = await getWeather(city);
-    return weather;
-  };
+  const city = args.join('+');
+  if (!city) {
+    return 'Usage: weather [city]. Example: weather casablanca';
+  }
+  const weather = await getWeather(city);
+  return weather;
+};
+
+export const getWeather = async (city: string): Promise<string> => {
+  try {
+    const response: AxiosResponse<string> = await axios.get(
+      `https://wttr.in/${city}?ATm`
+    );
+    return response.data;
+  } catch (error) {
+    return "error";
+  }
+};
