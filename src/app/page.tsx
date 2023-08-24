92947211690fdb97db61bb26a3366e30d6863a4a
Pieter Linde <pieterdlinde@gmail.com>
2023-08-24 21:28:40 +0200

    add computer icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ const LandingPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen ">
       <img className='absolute z-0' src="background.jpg" style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
+      <img className='absolute z-0' src='computer.png' style={{ left:'10px',top:'10px', width: '60px', height: '60px' }}></img>
       <div className="z-50">
         <TerminalPage />
       </div>
