8dd21356659fcc5efdb6d127f9f3706c4b13379b
Pieter Linde <Pieter.Linde@rr-wfm.com>
2023-08-24 16:59:14 +0200

    add background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import TerminalPage from './terminal-page';
 const LandingPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen ">
-    <div className="w-2/3 max-h-96" >
+      <img className='absolute z-0' src="background.jpg"  style={{width:'100%', height:'100%', objectFit: 'cover' }}></img>
+    <div className="w-2/3 h-96 z-50" >
       <TerminalPage />
     </div>
     </div>
