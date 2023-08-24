f465bb56f4c33e21c1932d1f24a5f077fce9ef43
Pieter Linde <pieterdlinde@gmail.com>
2023-08-24 21:31:05 +0200

    rename file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TerminalPage from './terminal-page';
 const LandingPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-screen h-screen ">
-      <img className='absolute z-0' src="background.jpg" style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
+      <img className='absolute z-0' src="background2.jpg" style={{ width: '100%', height: '100%', objectFit: 'cover' }}></img>
       <img className='absolute z-0' src='computer.png' style={{ left:'10px',top:'10px', width: '60px', height: '60px' }}></img>
       <div className="z-50">
         <TerminalPage />
