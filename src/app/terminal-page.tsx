84c7c180dacc012a4f2ff0d1e2f2c79adf3e9434
Pieter Linde <pieterdlinde@gmail.com>
2023-08-24 21:22:39 +0200

    Update to look like windows xp

--- src/app/terminal-page.tsx
+++ src/app/terminal-page.tsx
@@ -40,16 +40,26 @@ const TerminalPage: React.FC = () => {
 
     return (
         <>
+            <div className='text-light-foreground dark:text-dark-foreground rounded-t-md' style={{ borderColor: 'white', borderWidth: '5px', background: 'linear-gradient(90deg, rgba(1,84,229,1) 0%, rgba(32,117,242,1) 10%, rgba(56,178,254,1) 100%);' }}>
+                <div className='flex p-1'>
+                    <img src="cmd.png" style={{ marginRight: '7px', width: '30px', height: '25px' }}></img>
+                    C:\Resume\PieterLinde\resume\cmd.exe
+                    <div className='flex w-full origin-right justify-end'>
+                        <div className='flex items-center origin-center justify-center rounded-sm' style={{ backgroundColor: '#ECEAD8', width: '22px', fontWeight: 'bold', boxShadow:'0.5px 0.5px 0.5px 0.5px rgba(0,0,0,0.35)' }}>
+                            <img src="close.png" style={{ width: '15px', height: '15px', opacity:'0.6' }} ></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div
-                className="text-light-foreground dark:text-dark-foreground md:text-base rounded-md"
+                className="text-light-foreground dark:text-dark-foreground md:text-base rounded-b-md"
                 onClick={onClickAnywhere}
-                style={{height:'70vh', width:'70vw', borderWidth:'5px', boxShadow:'-4px -3px 45px 21px rgba(0,0,0,0.35)'}}
+                style={{ height: '70vh', width: '70vw', borderBottomWidth: '5px', borderLeftWidth: '5px', borderRightWidth: '5px', boxShadow: '-4px -3px 45px 21px rgba(0,0,0,0.35)' }}
             >
-                <main className="bg-light-background dark:bg-dark-background w-full h-full p-2 rounded-s">
+                <main className="bg-light-background dark:bg-dark-background w-full h-full p-0 rounded-s">
                     <div className="p-3 overflow-hidden h-full  rounded border-light-yellow dark:border-dark-yellow">
                         <div ref={containerRef} className="overflow-y-auto h-full">
                             <History history={history} />
-
                             <Input
                                 inputRef={inputRef}
                                 containerRef={containerRef}
