733f84acc619575682f37ae96e7a5e419c52b130
Pieter Linde <pieterdlinde@gmail.com>
2023-08-26 15:14:24 +0200

    Update text

--- src/app/data/commands/gui.ts
+++ src/app/data/commands/gui.ts
@@ -1,4 +1,5 @@
 export const gui = async (args: string[]): Promise<string> => {
-    window.open(`http://pieter-linde-profile.firebaseapp.com/`);
-    return `Opening profile gui page`;
+    // window.open(`http://pieter-linde-profile.firebaseapp.com/`);
+    // return `Opening profile gui page`;
+    return `Coming soon: Almost there, still a bit to do...`;
 };
